3641573ba5f4e42207ba8253b508fde9705a1ecb
Pallavi Agarwal <36764894+pallavimaruji@users.noreply.github.com>
2020-05-24 22:22:24 +0530

    Add files via upload

---
+++ ED.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+from keras.models import load_model
+
+emotion_dict = {0: "Angry", 1: "Disgust", 2: "Fear", 3: "Happy", 4: "Sad", 5: "Surprise", 6: "Neutral"}
+
+model = load_model('ED.h5')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    face_cascade = cv2.CascadeClassifier('myfacehaarcascade.xml')
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
+        roi_gray = gray[y:y + h, x:x + w]
+        yp = cv2.resize(roi_gray, (48, 48))
+        yp = np.expand_dims(yp,-1)
+        yp = np.expand_dims(yp,0)
+        prediction = model.predict([yp])
+        cv2.putText(frame, emotion_dict[int(np.argmax(prediction))], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+        
